cc1ba239b37702bc01fa10432bac2d3875b9289b
Marc Sherry <msherry@gmail.com>
2015-12-05 14:36:38 -0800

    NACHA mode, fixtures

---
+++ fxrd-autoloads.el
@@ -0,0 +1,7 @@
+(autoload 'tso6-mode "fxrd-mode" "TSO6 mode" t)
+(add-to-list 'auto-mode-alist '("\\.tso6\\($\\|\\.\\)" . tso6-mode))
+
+(autoload 'nacha-mode "fxrd-mode" "NACHA mode" t)
+(add-to-list 'auto-mode-alist '("\\.nacha\\($\\|\\.\\)" . nacha-mode))
+
+(provide 'fxrd-autoloads)
